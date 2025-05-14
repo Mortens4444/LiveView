9aa937c033aacb18e217af63de179fe07cbc16f5
Mortens <mortens.4444@gmail.com>
2025-05-14 22:15:02 +0200

    Add Wathdog usage batch file

---
+++ Watchdog/StartWatchdog.bat
@@ -0,0 +1,3 @@
+﻿REM Usage: Watchdog.exe ProcessPath [MinCpuUsage] [CheckIntervalMilliseconds]
+REM Watchdog.exe "C:\Program Files (x86)\Sziltech\LiveView\LiveView.exe" 0 1000
+REM Watchdog.exe "C:\Program Files (x86)\Sziltech\LiveView\LiveView.Agent.exe" 10 1000
