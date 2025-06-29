4777f6467d3a6c31785a2d80875f77a561c45bbd
Mortens <mortens.4444@gmail.com>
2025-06-29 22:20:41 +0200

    Installer bundle (one exe installer)

--- LiveView.Installer/Harvest files.bat
+++ LiveView.Installer/Harvest files.bat
@@ -1,10 +1,9 @@
 @echo off
 set heat="C:\Program Files (x86)\WiX Toolset v4.0\bin\heat.exe"
 
-set net9=..\LiveView\bin\Release\net9.0-windows
-set net462=..\LiveView\bin\Release\net462
+REM set lvFiles=..\LiveView\bin\Release\net9.0-windows
+set lvFiles=..\LiveView\bin\Release\net462
 
-%heat% dir "%net9%" -cg Net9Components -dr INSTALLFOLDER_NET9 -o Net9Components.wxs -gg -sreg -sfrag -var var.Net9Source
-%heat% dir "%net462%" -cg Net462Components -dr INSTALLFOLDER_NET462 -o Net462Components.wxs -gg -sreg -sfrag -var var.Net462Source
+%heat% dir "%lvFiles%" -cg LiveViewFiles -dr INSTALLFOLDER -o LiveViewFiles.wxs -srd -gg -sreg -sfrag -var var.LvFilesSource
 
 pause
